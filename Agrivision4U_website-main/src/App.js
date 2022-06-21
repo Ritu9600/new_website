4a5867f0f2423806d5129dc912903a13d53647f4
Ritu Patil <66620386+Ritu9600@users.noreply.github.com>
2022-06-21 17:21:37 +0530

    Add files via upload

---
+++ Agrivision4U_website-main/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Home from "./pages/Home.jsx";
+import TestSeries from "./pages/TestSeries.jsx";
+import Course from "./pages/Courses.jsx";
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+
+export default function App() {
+  return (
+    <Router>
+    <div>
+      <Routes>
+      <Route  path="/" exact component={Home} />
+      <Route path="/courses" exact component={Course} />
+      <Route path="/testseries" exact component={TestSeries} />
+      </Routes>
+    </div>
+    </Router>
+  );
+}
